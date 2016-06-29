34eeddbef91908338501552ce69c70fcfd642d95
McClelland Kemp <McClelland.Kemp@iriworldwide.com>
2016-06-29 14:57:28 -0500

    reduce on DT cols, is.defined and table comparison helpers

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,3 +1,4 @@
+.libPaths(c("/mapr/mapr03r/analytic_users/msmck/usr/local/lib/R", .libPaths())) 
 
 iri_week <- function(x, fmt = "%Y-%m-%d") {
   ceiling((as.numeric(as.Date(x, format = fmt)) + 25568)/7) - 4157
@@ -86,4 +87,74 @@ hread <- function(fp, schema, schema_loc) {
   data.table::setnames(stack, rcn)
   
   return(stack)
+}
+
+is.defined <- function(..., .all = FALSE) {
+  rs <- sapply(list(...), function(x) !is.null(x))
+  if (.all) return(all(rs)) else return(rs)
+}
+
+dt_reduce <- function(DT, FUN, NAME, ...) {
+  if (!requireNamespace("data.table", quietly = TRUE)) {
+    stop("`data.table` needed for this function to work. Please install it.", call. = FALSE)
+  } else {
+    # A super crude way to ensure the data.table .() works, need to fix once
+    # these functions are included in a package by refrencing properly in the namespace
+    require("data.table")
+  }
+  if (!is.data.table(DT)) setDT(DT)
+  COLNAMES <- c(...)
+  DT[, (NAME) := Reduce(FUN, .SD), .SDcols = (COLNAMES)]
+}
+
+dt_compare <- function(x, y, .names = NULL, names_x = NULL, names_y = NULL, 
+  all = TRUE, suffixes = NULL, FUN = `-`, .summary = TRUE, ...){
+  
+  if (!requireNamespace("data.table", quietly = TRUE)) {
+    stop("`data.table` needed for this function to work. Please install it.", call. = FALSE)
+  } else {
+    # A super crude way to ensure the data.table .() works, need to fix once
+    # these functions are included in a package by refrencing properly in the namespace
+    require("data.table")
+  }
+
+  if (!requireNamespace("magrittr", quietly = TRUE)) {
+    stop("`magrittr` needed for this function to work. Please install it.", call. = FALSE)
+  } else {
+    # A super crude way to ensure the magrittr pipe works, need to fix once
+    # these functions are included in a package by refrencing properly in the namespace
+    require("magrittr")
+  }
+
+  if (!is.data.table(x)) data.table::setDT(x)
+  if (!is.data.table(y)) data.table::setDT(y)
+
+  writeLines(paste(deparse(substitute(x)), "has", nrow(x), "rows"))
+  writeLines(paste(deparse(substitute(y)), "has", nrow(y), "rows"))
+
+  if (is.null(suffixes)) {
+    suffixes <- c(".x", ".y")
+  }
+
+  comp <- merge(x, y, all = all, suffixes = suffixes, ...)
+
+  if (is.defined(.names)) {
+    compare.vars <- lapply(suffixes, function(x) paste0(.names, x))
+  } else if (is.defined(names_x, names_y, .all = TRUE)) {
+    compare.vars <- list(
+      paste0(names_x, suffixes[1]),
+      paste0(names_y, suffixes[2])
+    )
+  } else stop("Must specify 'names' or both 'names_x' and 'names_y'...")
+
+  nms <- do.call(paste, c(compare.vars, sep = "_"))
+  for (ix in seq(nms)) {
+    xnm <- compare.vars[[1]][ix]
+    ynm <- compare.vars[[2]][ix]
+    nm <- nms[ix]
+    dt_reduce(DT = comp, FUN = FUN, NAME = nm, xnm, ynm)
+  }
+
+  print(comp[, summary(.SD), .SDcols = (nms)])
+  return(comp)
 }
